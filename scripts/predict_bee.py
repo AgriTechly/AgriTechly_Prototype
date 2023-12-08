9477794632c0f4dfc062717ce59b2554dd2788b1
unknown <a.e.k426rahmani@gmail.com>
2023-12-08 22:14:46 +0100

    import libraries bee

---
+++ scripts/predict_bee.py
@@ -0,0 +1,15 @@
+# by @aek426rahmani
+# 08-12-2023
+"""
+this file is used to predict and return the result in results/result[i].json file the i is the number of the result to save history of the results
+it will be executed by the server
+"""
+
+# import the necessary packages
+import sys
+import os
+import json
+import cv2
+import tensorflow as tf
+import numpy as np
+
