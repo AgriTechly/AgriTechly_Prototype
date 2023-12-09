92ed7f62a787cad6916d77c89058a65d5ecd9a11
unknown <a.e.k426rahmani@gmail.com>
2023-12-09 14:01:10 +0100

    adding submitform in js

--- dist/script.js
+++ dist/script.js
@@ -28,3 +28,33 @@ function activateImageInput(inputId) {
       imgElement.src = URL.createObjectURL(selectedFile);
     }
   }
+
+  function submitForm() {
+    // Get the form data
+    var formData = new FormData(document.forms[0]);
+
+    // Create a new XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
+
+    // Configure it: POST-request for the process.php script
+    xhr.open('POST', 'process.php', true);
+
+    // Set up the callback function to handle the response
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Parse the JSON response
+            var response = JSON.parse(xhr.responseText);
+
+            // Update the page with the result or error
+            if (response.result) {
+                alert('Image Classification Result: ' + response.result);
+                // You can update the page content dynamically here
+            } else if (response.error) {
+                alert('Error: ' + response.error);
+            }
+        }
+    };
+
+    // Send the form data
+    xhr.send(formData);
+}
