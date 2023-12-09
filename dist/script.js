7a53312ebf24c35fb0973528ada402beeddac0f3
unknown <a.e.k426rahmani@gmail.com>
2023-12-09 13:36:54 +0100

    bee running script

--- dist/script.js
+++ dist/script.js
@@ -32,3 +32,33 @@ function activateImageInput(inputId) {
   });
 
   }
+
+  function handleBeeImageUpload(input) {
+    const selectedFile = input.files[0];
+
+    if (selectedFile) {
+      // You can handle the selected file here
+      console.log("Selected File:", selectedFile);
+
+      // You may want to display the selected image
+      const imgElement = input.previousElementSibling.previousElementSibling; // Assuming the img element is two elements before the input
+      imgElement.src = URL.createObjectURL(selectedFile);
+    }
+  }
+
+  function runBeeScript(input){
+    const { exec } = require('child_process');
+
+  // Replace 'your_script.py' with the actual name of your Python script
+  const pythonScript = '../scripts/predict_bee.py';
+
+  exec(`python ${pythonScript} ${input}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing the Python script: ${error}`);
+      return;
+    }
+
+    console.log(`Python script output:\n${stdout}`);
+  });
+
+  }
