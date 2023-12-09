f2a5028bd689579201b894e4018fa37ec9e30a72
unknown <a.e.k426rahmani@gmail.com>
2023-12-09 14:00:43 +0100

    changing forms

--- dist/script.js
+++ dist/script.js
@@ -13,27 +13,9 @@ function activateImageInput(inputId) {
       // You may want to display the selected image
       const imgElement = input.previousElementSibling.previousElementSibling; // Assuming the img element is two elements before the input
       imgElement.src = URL.createObjectURL(selectedFile);
-      runPlantScript(imgElement.src);
     }
   }
-
-  function runPlantScript(input){
-    const { exec } = require('child_process');
-
-  // Replace 'your_script.py' with the actual name of your Python script
-  const pythonScript = '../scripts/predict_plant.py';
-
-  exec(`python ${pythonScript} ${input}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing the Python script: ${error}`);
-      return;
-    }
-
-    console.log(`Python script output:\n${stdout}`);
-  });
-
-  }
-
+  
   function handleBeeImageUpload(input) {
     const selectedFile = input.files[0];
 
@@ -44,23 +26,5 @@ function activateImageInput(inputId) {
       // You may want to display the selected image
       const imgElement = input.previousElementSibling.previousElementSibling; // Assuming the img element is two elements before the input
       imgElement.src = URL.createObjectURL(selectedFile);
-      runBeeScript(imgElement.src);
     }
-  }
-
-  function runBeeScript(input){
-    const { exec } = require('child_process');
-
-  // Replace 'your_script.py' with the actual name of your Python script
-  const pythonScript = '../scripts/predict_bee.py';
-
-  exec(`python ${pythonScript} ${input}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing the Python script: ${error}`);
-      return;
-    }
-
-    console.log(`Python script output:\n${stdout}`);
-  });
-
   }
