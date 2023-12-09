5264130544138be2a63393a87e345952d80395ef
unknown <a.e.k426rahmani@gmail.com>
2023-12-09 13:36:04 +0100

    palnt running script

--- dist/script.js
+++ dist/script.js
@@ -3,7 +3,7 @@ function activateImageInput(inputId) {
     imageInput.click();
   }
 
-  function handleImageUpload(input) {
+  function handlePlantImageUpload(input) {
     const selectedFile = input.files[0];
 
     if (selectedFile) {
@@ -14,4 +14,21 @@ function activateImageInput(inputId) {
       const imgElement = input.previousElementSibling.previousElementSibling; // Assuming the img element is two elements before the input
       imgElement.src = URL.createObjectURL(selectedFile);
     }
+  }
+
+  function runPlantScript(input){
+    const { exec } = require('child_process');
+
+  // Replace 'your_script.py' with the actual name of your Python script
+  const pythonScript = '../scripts/predict_plant.py';
+
+  exec(`python ${pythonScript} ${input}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing the Python script: ${error}`);
+      return;
+    }
+
+    console.log(`Python script output:\n${stdout}`);
+  });
+
   }
