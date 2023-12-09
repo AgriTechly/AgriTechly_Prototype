125c04b2411dd13e3f38a745094566f370387efc
unknown <a.e.k426rahmani@gmail.com>
2023-12-09 20:00:46 +0100

    import libraries server

--- dist/server/server.py
+++ dist/server/server.py
@@ -3,3 +3,9 @@
 """
 this file is the server of the project
 """
+
+# import the necessary packages
+from flask import Flask, request, jsonify, render_template
+import subprocess
+import json
+import os
